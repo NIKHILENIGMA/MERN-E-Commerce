8eb77732acf5ff957d1c5c1726fb39c231fcafe4
nikhilenigma <nickharmalkar18@gmail.com>
2024-02-13 00:52:25 +0530

    fix: :art: Improve middleware structure to resolve the issue
    First asyncHandler is not export default second one check is add which confirms that token value do not contain any undefined or null value

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import asyncHandler  from "../utils/asyncHandler.js";
+import { asyncHandler }  from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
@@ -17,31 +17,36 @@ import { User } from "../models/user.model.js";
  * @throws {ApiError} If the JWT is not provided, invalid, or the user does not exist.
  */
 
-export const verifyJWT = asyncHandler(async(req, _, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request")
-        }
-    
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        
+    //   console.log("Token value",req.cookies?.accessToken);
+      const token =
+        req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ","");
+  
+      if (!token) {
+        throw new ApiError(401, "Unauthorized request in authentication");
+      }
 
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-        console.log("user: ", user);
-    
-        if (!user) {
-            
-            throw new ApiError(401, "Invalid Access Token")
-        }
-    
-        req.user = user;
-        next();
+      if (typeof token !== 'string') {
+        throw new ApiError(400, "Token must be a string")
+      }
+  
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  
+    //   console.log(`Middleware :: Decoded header:${JSON.stringify(`ID : ${decodedToken?._id},username : ${decodedToken?.username}`)} `);
+  
+      const user = await User.findById(decodedToken?._id).select(
+        "-password -refreshToken"
+      );
+  
+      if (!user) {
+        throw new ApiError(401, "Invalid Access token");
+      }
+      //@ Custome middleware 
+      req.user = user
+      next()
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token")
-    }
-    
-})
-
-// console.log(verifyJWT.token);
+      throw new ApiError(401, error?.message || "Invalid access token")
+    } 
+  });
