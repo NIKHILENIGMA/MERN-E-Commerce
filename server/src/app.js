8e14e56f323fb6afe7c63eb1f8999dfc261755d5
nikhilenigma <nickharmalkar18@gmail.com>
2024-02-12 23:27:02 +0530

    feat: :wrench: Setup App.js with middlrwares and routes
    Add  cookie-parse, cors also add some options and setup routes for user

---
+++ server/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+let corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}
+
+
+/// All Middleware congfigurations 
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static('public'))
+app.use(cors(corsOptions))
+app.use(cookieParser())
+
+
+/// Api routes
+import userRouters from "./routes/user.route.js"
+
+app.use("/api/v1/users", userRouters);
+
+
+
+
+
+export {app};
