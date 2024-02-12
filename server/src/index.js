bce217dee808213e9b5eb16f17024ef90a86b9a7
nikhilenigma <nickharmalkar18@gmail.com>
2024-02-12 23:29:42 +0530

    feat: :card_file_box: Mongo DB connection setup
    ConnectDb and manage its promises

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv";
 import connnectDb from "../src/db/db.js"
+import { app } from "./app.js";
 
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
+const port = process.env.PORT || 5000;
 connnectDb()
+.then(()=>{
+    app.listen(port,()=> {
+        console.log(`Server is running at port: ${port}`);
+    })
+})
+.catch((err)=> {
+    console.log("Mongo db connection is failed :: Error: ", err);
+})
