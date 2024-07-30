5e99dd274723769c6ba8c297933db2059253198b
nikhilenigma <nickharmalkar18@gmail.com>
2024-07-30 16:18:24 +0530

    Update category controllers like create, update, delete, all and single
    - Include the current status logic in create controller also add some more informative response to user to know which category has been created.
    - Include the current status logic in update controller also add some checks for name and status.
    - Improve error messages for more clear and helpful for detecting the error in all the controllers
    - In single and all controllers just removing the updateAt time in response

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -3,187 +3,188 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+/// Create Category Controller
+export const createCategory = asyncHandler(async (req, res) => {
+  // Extracting the name and status from req body
+  const { name, status } = req.body;
 
-const createCategory = asyncHandler(async ( req, res ) => {
-    try {
-        const { name } = req.body
-
-        // Check if category name is provided or not  
-        if (!name) {
-            throw new ApiError (401, "Category name is required!!")
-        }
-
-        // Check if category is already created or not
-        const existingCategory = await Category.findOne({name})
-        if (existingCategory) {
-            throw new ApiError (409, "Category already exists!")
-        }
-
-        const category = await Category.create({
-            name
-        });
-
-        console.log(`Category created successfully: ${category.name}`); 
-        
-        res
-        .status(201)
-        .json(
-            new ApiResponse(
-                201,
-                category,
-                "Category created successfully"
-            )
-        )
-    
-
-    } catch (error) {   
-        throw new ApiError(
-            401,
-            error?.message || "Category not created"
-        )
+  try {
+    // Check if category name is provided or not
+    if (!name) {
+      throw new ApiError(401, "Category name is required!!");
     }
-})
 
-const updateCategory = asyncHandler(async (req, res) => {
-    // Extrating the category name and Id from the express request object 
-    
-    
-    try {
-        const { name }= req.body
-        const { id: categoryID} = req.params
- 
+    // Check if category is already created or not
+    const existingCategory = await Category.findOne({ name });
 
-        if (!name) {
-            throw new ApiError(
-                400,
-                "Category name is required!"
-            )
-        }
-    
-        // Find the category by its ID
-        const updatedCategory = await Category.findByIdAndUpdate(
-            categoryID,
-            {
-                $set: {
-                    name
-                },
-            },
-            {
-               new: true 
-            }
+    if (existingCategory) {
+      throw new ApiError(
+        409,
+        `The category by name of ${existingCategory.name} already exist please try again with different category name.`
+      );
+    }
+
+    const category = await Category.create({
+      name,
+      status,
+    });
+
+    console.log(
+      `At 32, In category controller, created category details: ${category}`
+    );
+
+    res
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          category,
+          `The category with name of ${category.name} has been created successfully`
         )
-        
-        console.log(`${name} category name updated to:  ${updatedCategory.name}`)
-        res
-        .status(200)
-        .json(
-            new ApiResponse(
-                200,
-                updatedCategory,
-                "Category updated succefully!"
-            )
-        )
-} catch (error) {
-    throw new ApiError(
-        500, 
-        "Catergory updation failed" ||  error?.message
-    )
-}
-
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Category not created");
+  }
 });
 
+/// Update category controller
+export const updateCategory = asyncHandler(async (req, res) => {
+  // Extrating name and status from req body and ID of the category from req parameter
+  const { name, status } = req.body;
+  const { id: categoryID } = req.params;
 
-const destroyCategory = asyncHandler(async ( req, res ) => {
-    const { id } = req.params
+  try {
+    if (!(name || status)) {
+      throw new ApiError(
+        404,
+        "Name of the category or status of category do not exist. Please try again later."
+      );
+    }
 
-    try {
-        if (!id) {
-            throw new ApiError(
-                400,
-                "Please send a valid id",
-            )
-        }
-    
-        const deletedCategory = await Category.findByIdAndDelete(id)
-        
-        if (!deletedCategory) {
-            throw new ApiError(
-                404,
-                "Category not found"
-                )
-        }
-        console.log(`Deleted category: ${deletedCategory.name}`);
-        res
-        .status(200)
-        .json(
-            new ApiResponse(
-                200,
-                "Category deleted successfully!!"
-            )
-        )
-} catch (error) {
+    // Find the category by its ID and then update the category details
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryID,
+      {
+        $set: {
+          name,
+          status,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedCategory) {
+      throw new ApiError(
+        404,
+        "The category you are trying to update does not exist."
+      );
+    }
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, 
+            updatedCategory, 
+            "Category details updated successfully"
+        ));
+
+  } catch (error) {
     throw new ApiError(
-        500, 
-        error?.message || "Category deletion failed");
-    
-}
-})
+      500,
+      error?.message ||
+        "There was an error while updating the category. Please try again later"
+    );
+  }
+});
 
-const listCategories = asyncHandler(async (_, res) => {
-    const allCategories = await Category.find({})
+/// Delete category controller
+export const destroyCategory = asyncHandler(async (req, res) => {
+  // Extracting the id from the URL
+  const { id } = req.params;
 
-    if( !allCategories || allCategories.length === 0){ 
-        throw new ApiError(
-            404,
-            "No Categories found"
-        )
+  // If ID does not get from the params then throw new Error
+  if (!id) {
+    throw new ApiError(
+      400,
+      "The ID which you are access might be deleted or do not exist"
+    );
+  }
+
+  try {
+    // Find the category by ID and then destroy it from database
+    const deletedCategory = await Category.findByIdAndDelete(id);
+
+    if (!deletedCategory) {
+      throw new ApiError(
+        404,
+        "The category you are trying to remove does not exist or already remove"
+      );
     }
+    console.log(`At line 131, In delete category: ${deletedCategory}`);
 
     res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          `The ${deletedCategory.name} category has been removed successfully`
+        )
+      );
+  } catch (error) {
+    throw new ApiError(500, error?.message || "Category deletion failed");
+  }
+});
+
+/// Get all the categories controller
+export const listAllCategories = asyncHandler(async (_, res) => {
+  const allCategories = await Category.find({}).select("-updatedAt");
+
+  if (!allCategories || allCategories.length === 0) {
+    throw new ApiError(
+        404, 
+        "Something went wrong while retriving the all the category"
+    );
+  }
+
+  res
     .status(200)
     .json(
-        new ApiResponse(
-            200,
-            allCategories,
-            "Successfully fetched all the categories!"
-        )
-    )
-})
+      new ApiResponse(
+        200,
+        allCategories,
+        "Successfully fetched all the categories!"
+      )
+    );
+});
 
-const readCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params
+/// Get a category controller
+export const readCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
-    if (!id) {
-        throw new ApiError(
-            400,
-            "ID is required exist"
-        )
-    }
+  if (!id) {
+    throw new ApiError(
+        400, 
+        "ID is required exist"
+    );
+  }
 
-    const getCategory = await Category.findById(id).select('-createdAt -updatedAt')
+  const getCategory = await Category.findById(id).select(
+    "-updatedAt"
+  );
 
-    if (!getCategory) {
-        throw new ApiError(
-            404,
-            "Category not found",
-        )
-    }
-    res
+  if (!getCategory) {
+    throw new ApiError(404, "Category not found");
+  }
+  res
     .status(200)
     .json(
-        new ApiResponse(
-            200,
-            getCategory,
-            "Successfully retrieved the specified category"
-        )
-    )
-})
-
-
-export {
-    createCategory,
-    updateCategory,
-    destroyCategory,
-    listCategories,
-    readCategory,
-}
+      new ApiResponse(
+        200,
+        getCategory,
+        "Successfully retrieved the specified category"
+      )
+    );
+});
