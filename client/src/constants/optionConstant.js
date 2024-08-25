94939554e71905c075a071a85620ba57ceb9893c
nikhilenigma <nickharmalkar18@gmail.com>
2024-08-25 19:01:51 +0530

    feat: Add constants for category, product status, and user options

---
+++ client/src/constants/optionConstant.js
@@ -0,0 +1,9 @@
+/**
+ * @description: This file contains the options.
+ */
+
+export const categoryOptions = ["5", "10", "15", "20"];
+
+export const productStatusOptions = ["Active", "Inactive", "Draft"];
+
+export const userOptions = ["5", "10", "15", "20"];
