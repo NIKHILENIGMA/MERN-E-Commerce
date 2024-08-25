3bfd835eb708673f7d28ae742f43d6574dddf9a3
nikhilenigma <nickharmalkar18@gmail.com>
2024-08-25 16:13:57 +0530

    feat: Add initial values for product and category forms
    - Initial values are added for the product and category forms to provide default values for each field.
    - The product form includes fields like title, sku, barCode, description, price, discountPrice, countInStock, category, status, tags, quantity, and image.
    - The category form includes fields like name, description, image, and status.

---
+++ client/src/constants/initialValues.js
@@ -0,0 +1,22 @@
+export const productInitialValues = {
+  title: "",
+  sku: "",
+  barCode: "",
+  description: "",
+  price: "",
+  discountPrice: "",
+  countInStock: "",
+  category: "",
+  status: "",
+  tags: "",
+  quantity: "",
+  image: null,
+};
+
+export const categoryInitialValues = {
+  name: "",
+  description: "",
+  image: "",
+  status: "",
+};
+
