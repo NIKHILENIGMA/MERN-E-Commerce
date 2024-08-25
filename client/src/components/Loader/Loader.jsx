1fe140931507a7509f79a29f9a519716fc2b5d97
nikhilenigma <nickharmalkar18@gmail.com>
2024-08-25 16:05:34 +0530

    feat: Add Loader component for displaying loading animation

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-16 h-16 border-t-4 border-black border-solid rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Loader
